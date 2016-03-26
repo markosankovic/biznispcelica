9e6c18fc78eb7d41f0388bb54abfe520f2305d48
Marko Sanković <sankovic.marko@gmail.com>
2016-03-26 17:21:43 +0100

    Initial version of site

---
+++ js/contact_me.js
@@ -0,0 +1,33 @@
+$(function() {
+
+  $("input,textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      // Success message
+      $('#success').html("<div class='alert alert-success'>");
+      $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        .append("</button>");
+      $('#success > .alert-success')
+        .append("<strong>Vaša poruka je poslata. </strong>");
+      $('#success > .alert-success')
+        .append('</div>');
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  $('#success').html('');
+});
